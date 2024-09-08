9bc83329e6468c0aeca4f27aac5f19eb876bd1b7
Joaco <lopezjoaquin488@gmail.com>
2024-09-08 15:24:39 -0300

    act

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -1,38 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class ButtonController : Controller
 {
     Vector3 dir;
-    [SerializeField] Button upButton;
-    [SerializeField] Button rightButton;
-    [SerializeField] Button leftButton;
+    [SerializeField] EventTrigger upButton;
+    [SerializeField] EventTrigger rightButton;
+    [SerializeField] EventTrigger leftButton;
 
     private void Start()
     {
-        upButton.onClick.AddListener(new UnityEngine.Events.UnityAction(PressUpButton));
+        /*upButton.onClick.AddListener(new UnityEngine.Events.UnityAction(PressUpButton));
         rightButton.onClick.AddListener(new UnityEngine.Events.UnityAction(PressRightButton));
-        leftButton.onClick.AddListener(new UnityEngine.Events.UnityAction(PressLeftButton));
+        leftButton.onClick.AddListener(new UnityEngine.Events.UnityAction(PressLeftButton));*/
     }
     public override Vector3 GetMoveDir()
     {
         return dir;
     }
 
-    void PressUpButton()
+    public void PressUpButton()
     {
-        dir = Vector3.forward;
+        dir = Vector3.up;
     }
 
-    void PressRightButton()
+    public void PressRightButton()
     {
         dir = Vector3.right;
     }
 
-    void PressLeftButton()
+    public void PressLeftButton()
     {
         dir = -Vector3.right;
+    }
+
+    public void stopMovement()
+    {
+        dir = Vector3.zero;
     }
 }
