7c58a576745ee399fcc8f738c59b15dcd8b625d0
Augus <145805991+au6us@users.noreply.github.com>
2024-09-30 11:47:46 -0300

    Revert "listorti las frutitas curativas"
    This reverts commit 4a7142434ae2aaede822be221c0ebc77f38e0820.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -267,20 +267,6 @@ public class Player : MonoBehaviour
         }
     }
 
-    public void Curar(int cantidadCuracion)
-    {
-        life += cantidadCuracion;
-
-        // Limitar la vida para que no sobrepase el máximo
-        if (life > maxLife)
-        {
-            life = maxLife;
-        }
-
-        // Actualizar la barra de vida
-        barraDeVida.CambiarVidaActual(life);
-    }
-
     private void Dead()
     {
         Time.timeScale = 0;
