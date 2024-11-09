e524807c90d1fb84d49321dd6f6c78ce19ec82d9
Augus <145805991+au6us@users.noreply.github.com>
2024-11-09 17:54:05 -0300

    sigo

--- Assets/Scripts/Menu/StaminaSystem.cs
+++ Assets/Scripts/Menu/StaminaSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,50 @@ using UnityEngine;
 public class StaminaSystem : MonoBehaviour
 {
     public int currentStamina; //El encargado de la que stamina baje
+
+    public float timer;
+
     [SerializeField] private int maxStamina = 10;
     [SerializeField] private float timeToChargeStamina = 10f;
 
+    DateTime nextStaminaTime; 
+
     public bool recharging;
+
+    IEnumerator AutoRechargeStamina()
+    {
+        recharging = true;
+        timer = 0;
+
+        while (currentStamina < maxStamina)
+        {
+            DateTime currentTime = DateTime.Now;
+            DateTime nextTime = nextStaminaTime;
+
+            while(currentTime > nextTime)
+            {
+                if (currentStamina >= maxStamina) break;    
+
+                currentStamina += 1;
+
+                nextTime = nextTime.AddSeconds(timeToChargeStamina);
+            }
+
+            //timer += Time.deltaTime;
+
+            //if(timer >= timeToChargeStamina)
+            //{
+            //    currentStamina += 1;
+            //    timer = 0;
+            //}
+
+            yield return new WaitForEndOfFrame();
+
+        }
+
+        recharging = false;
+
+    }
 
     public bool UseStamina(int stamina)
     {
@@ -17,9 +58,10 @@ public class StaminaSystem : MonoBehaviour
 
         currentStamina -= stamina;
 
-        if(!!recharging && currentStamina < maxStamina)
+        if(!recharging && currentStamina < maxStamina)
         {
-            recharging = true;
+            nextStaminaTime = DateTime.Now.AddSeconds(timeToChargeStamina);
+            StartCoroutine(AutoRechargeStamina());
         }
         return true;
     }
@@ -27,5 +69,16 @@ public class StaminaSystem : MonoBehaviour
     public void RechargeStamina(int stamina)
     {
         currentStamina += stamina;
+
+        if(recharging && currentStamina >= maxStamina)
+        {
+            StopAllCoroutines();
+            recharging = false;
+        }
+    }
+
+    public void SaveStamina()
+    {
+
     }
 }
