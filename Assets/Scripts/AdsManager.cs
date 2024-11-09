9656bd5641f3fceab3f1785bb5d6b5b33aed3ffd
Augus <145805991+au6us@users.noreply.github.com>
2024-11-09 20:21:40 -0300

    boton ads

--- Assets/Scripts/AdsManager.cs
+++ Assets/Scripts/AdsManager.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Advertisements;
 
-public class AdsManager : MonoBehaviour
+public class AdsManager : MonoBehaviour, IUnityAdsInitializationListener, IUnityAdsShowListener
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] string gameID = "5728142";
+
+    [SerializeField] string adID = "Interstitial_Android";
+
+    public void OnInitializationComplete()
     {
-        
+        Advertisement.Load(adID);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
     {
-        
+
+    }
+
+    private void Start()
+    {
+        Advertisement.Initialize(gameID, true, this);
+    }
+
+    public void ShowAD()
+    {
+        if (!Advertisement.isInitialized) return;
+
+        Advertisement.Show(adID, this);
+    }
+
+    public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
+    {
+    }
+
+    public void OnUnityAdsShowStart(string placementId)
+    {
+    }
+
+    public void OnUnityAdsShowClick(string placementId)
+    {
+    }
+
+    public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
+    {
+
+        if(showCompletionState == UnityAdsShowCompletionState.COMPLETED)
+        {
+            Debug.Log("¡Obtuviste una carga de Stamina!");
+        }
+        else
+        {
+            Debug.Log("No obtuviste tu carga de Stamina :(");
+        }
+
     }
 }
