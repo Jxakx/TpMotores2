f849e1c1c980397d725156a8682bb2ad1c875a56
Joaco <lopezjoaquin488@gmail.com>
2025-03-03 12:35:37 -0300

    Estrellas funca

--- Assets/Scripts/Menu/MenuLevels.cs
+++ Assets/Scripts/Menu/MenuLevels.cs
@@ -2,26 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuLevels : MonoBehaviour
 {
-    private Player player;
+    private int totalStars; // Para almacenar las estrellas guardadas
+    private string saveFilePath;
+    public Button levelTwoButton; // Botón del nivel 2
 
-    public void Start()
+    private void Start()
     {
-        FindObjectOfType<Player>();
+        saveFilePath = Application.persistentDataPath + "/level_data.json";
+        LoadStarData();
+
+        // Desactivar el botón del Nivel 2 si no tiene suficientes estrellas
+        if (levelTwoButton != null)
+        {
+            levelTwoButton.interactable = totalStars >= 2;
+        }
     }
+
+    private void LoadStarData()
+    {
+        if (System.IO.File.Exists(saveFilePath))
+        {
+            string json = System.IO.File.ReadAllText(saveFilePath);
+            LevelData levelData = JsonUtility.FromJson<LevelData>(json);
+
+            totalStars = 0;
+            foreach (int stars in levelData.starCounts)
+            {
+                totalStars += stars; // Sumar todas las estrellas obtenidas
+            }
+        }
+        else
+        {
+            totalStars = 0; // Si no hay datos, iniciar con 0 estrellas
+        }
+    }
+
     public void LevelOne()
     {
         SceneManager.LoadScene(1);
         Time.timeScale = 1;
-        //player.ResetPlayerCollisions();
     }
 
     public void LevelTwo()
     {
-        SceneManager.LoadScene(2);
-        Time.timeScale = 1;
-        //player.ResetPlayerCollisions();
+        if (totalStars >= 2)
+        {
+            SceneManager.LoadScene(2);
+            Time.timeScale = 1;
+        }
+        else
+        {
+            Debug.Log("No tienes suficientes estrellas para desbloquear el Nivel 2.");
+        }
     }
 }
