d2e03c073ccba9ae00265e41e891285b03c37e4a
Jxakx <lopezjoaquin488@gmail.com>
2026-02-03 19:19:43 -0300

    controles

--- Assets/Scripts/Player/ButtonController.cs
+++ Assets/Scripts/Player/ButtonController.cs
@@ -6,97 +6,111 @@ using UnityEngine.UI;
 
 public class ButtonController : Controller
 {
-    Vector3 dir;
-    [SerializeField] EventTrigger jumpButton;
-    [SerializeField] EventTrigger dashButton;
-    [SerializeField] EventTrigger rightButton;
-    [SerializeField] EventTrigger leftButton;
+    private Vector3 dir;
     private bool jump = false;
     private bool dash = false;
 
+    // --- VARIABLES NUEVAS PARA GUARDAR EL ESTADO ---
+    private bool moviendoDerecha = false;
+    private bool moviendoIzquierda = false;
+
     private void Start()
     {
-        int currentLevel = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex; // Obtener el nivel actual
-
+        int currentLevel = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
         if (currentLevel == 2)
         {
-            SetDashButtonState(FindObjectOfType<JSONSaveHandler>().LoadDashState());
+            var saveHandler = FindObjectOfType<JSONSaveHandler>();
+            if (saveHandler != null)
+            {
+                SetDashButtonState(saveHandler.LoadDashState());
+            }
         }
     }
+
     private void Update()
     {
-        //Inputs para PC
-        if (Input.GetKey(KeyCode.A)) // Moverse a la izquierda
+        // 1. Detectamos Teclado (PC)
+        bool tecladoD = Input.GetKey(KeyCode.D);
+        bool tecladoA = Input.GetKey(KeyCode.A);
+
+        // 2. Combinamos: Se mueve si apretás Tecla O si mantenés el Botón Táctil
+        float horizontalInput = 0;
+
+        if (tecladoD || moviendoDerecha)
         {
-            PressLeftButton();
+            horizontalInput = 1;
         }
-        else if (Input.GetKey(KeyCode.D)) // Moverse a la derecha
+        else if (tecladoA || moviendoIzquierda)
         {
-            PressRightButton();
-        }
-        else
-        {
-            stopMovement();
+            horizontalInput = -1;
         }
 
-        if (Input.GetKeyDown(KeyCode.W)) // Saltar
-        {
-            PressJumpButton();
-        }
+        dir = new Vector3(horizontalInput, 0, 0);
 
-        if (Input.GetKeyDown(KeyCode.S)) // Dash
-        {
-            PressDashButton();
-        }
-        
+        // 3. Inputs de acción (W y S)
+        if (Input.GetKeyDown(KeyCode.W)) PressJumpButton();
+        if (Input.GetKeyDown(KeyCode.S)) PressDashButton();
     }
 
+    // --- MÉTODOS PARA LOS BOTONES TÁCTILES ---
+
+    // Llamar a este en "Pointer Down" del botón Derecha
+    public void PressRightButton()
+    {
+        moviendoDerecha = true;
+    }
+
+    // Llamar a este en "Pointer Up" del botón Derecha
+    public void ReleaseRightButton()
+    {
+        moviendoDerecha = false;
+    }
+
+    // Llamar a este en "Pointer Down" del botón Izquierda
+    public void PressLeftButton()
+    {
+        moviendoIzquierda = true;
+    }
+
+    // Llamar a este en "Pointer Up" del botón Izquierda
+    public void ReleaseLeftButton()
+    {
+        moviendoIzquierda = false;
+    }
+
+    public void stopMovement() // Por si algún otro script lo usa
+    {
+        moviendoDerecha = false;
+        moviendoIzquierda = false;
+    }
+
+    // --- MÉTODOS OBLIGATORIOS DEL CONTROLLER ---
     public override Vector3 GetMoveDir()
     {
-        return dir != Vector3.zero ? dir : Vector3.zero;
+        return dir;
     }
 
     public override bool IsJumping()
     {
         bool wasJumping = jump;
-        jump = false;  // Restablecemos el estado de salto después de evaluarlo
+        jump = false;
         return wasJumping;
     }
 
     public override bool IsDashing()
     {
         bool wasDashing = dash;
-        dash = false;  // Restablecemos el estado de salto después de evaluarlo
+        dash = false;
         return wasDashing;
     }
-    public void PressJumpButton()
-    {
-        jump = true;
-    }
 
-    public void PressDashButton()
-    {
-        dash = true;
-    }
-    public void PressRightButton()
-    {
-        dir = Vector3.right;
-    }
+    public void PressJumpButton() { jump = true; }
+    public void PressDashButton() { dash = true; }
 
-    public void PressLeftButton()
+    public void SetDashButtonState(bool unlocked)
     {
-        dir = -Vector3.right;
-    }
-
-    public void stopMovement()
-    {
-        dir = Vector3.zero;
-        //jump = false;
-        dash = false;
-    }
-
-    public void SetDashButtonState(bool isEnabled)
-    {
-        dashButton.enabled = isEnabled;
+        // Aquí podés activar/desactivar visualmente el botón de dash
+        GameObject btnDash = GameObject.Find("DashButtonName"); // Cambiá por el nombre real
+        if (btnDash != null) btnDash.SetActive(unlocked);
     }
 }
